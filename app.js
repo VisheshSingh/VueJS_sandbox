92a56b7a2cb1e897ce2ce55c7a0ea6f46e9af4db
Vishesh <visheshsingh16@gmail.com>
2018-09-08 18:11:59 -0500

    Events

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ new Vue({
     name: "Vishesh",
     job: "Software developer",
     website: "http://www.thenetninja.co.uk",
-    websiteTag: '<a href="http://www.google.com">Google!</a>'
+    websiteTag: '<a href="http://www.google.com">Google!</a>',
+    age: 25
   },
   methods: {
     greet: function(time) {
       return "Good " + time + " " + this.name;
+    },
+    add: function(inc) {
+      this.age += inc;
+    },
+    subtract: function(dec) {
+      this.age -= dec;
     }
   }
 });
